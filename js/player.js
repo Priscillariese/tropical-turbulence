27cb51dfaf44b38c3a983e54fc52f07aa8fbb36f
Priscilla Riesegit config --global user.email priscilla.riese@gmail.com <priscilla.riese@gmail.com>
2023-06-26 12:52:29 +0200

    atualizado

--- js/player.js
+++ js/player.js
@@ -48,19 +48,22 @@ class Player {
         this.element.style.left = `${this.left}px`
     }
   
-    didCollide(obstacle) {
-        const playerRect = this.element.getBoundingClientRect();
-        const obstacleRect = obstacle.element.getBoundingClientRect();
-  
-        if (
-            playerRect.left < obstacleRect.right &&
-            playerRect.right > obstacleRect.left &&
-            playerRect.top < obstacleRect.bottom &&
-            playerRect.bottom > obstacleRect.top
-        ) {
-            return true;
-        } else {
-            return false;
+
+        didCollide(obstacle) {
+                const playerRect = this.element.getBoundingClientRect();
+                const obstacleRect = obstacle.element.getBoundingClientRect();
+        
+                if (
+                    playerRect.left < obstacleRect.right &&
+                    playerRect.right > obstacleRect.left &&
+                    playerRect.top < obstacleRect.bottom &&
+                    playerRect.bottom > obstacleRect.top
+                ) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         }
-    }
-  }
+
+  
